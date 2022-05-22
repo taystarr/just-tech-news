c3c2b057c0adf86fb66c794145fd1f7e74bdfb3a
Tay Starr <tstarrroberts@gmail.com>
2022-05-22 10:33:55 -0400

    add semi functioning log in and log out

--- config/connection.js
+++ config/connection.js
@@ -10,3 +10,18 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 });
 
 module.exports = sequelize;
+
+// const Sequelize = require('sequelize');
+
+// require('dotenv').config();
+
+// // create connection to our db
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     });
+
+// module.exports = sequelize;
